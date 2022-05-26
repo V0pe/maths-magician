2791e4acc38bd56986dbb31d401d42b388584c89
V0pe <dampblew01@gmail.com>
2022-05-26 10:44:35 -0400

    added the event handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.PureComponent {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return <Calculator />;
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prefer-stateless-function
 import React from 'react';
 import './Calculator.css';
-import Calculate from './logic/calculate';
+import Calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -27,30 +27,30 @@ class Calculator extends React.Component {
 
     return (
       <main>
-        <div className="item result">
+        <div id="result" className="item result">
           {total}
           {operation}
           {next}
         </div>
-        <button type="button" className="item reset" name="AC"> AC </button>
-        <button type="button" className="item order" name="+/-"> +/- </button>
-        <button type="button" className="item percent" name="%"> % </button>
-        <button type="button" className="item divide" name="÷"> / </button>
-        <button type="button" className="item no-7" name="7"> 7 </button>
-        <button type="button" className="item no-8" name="8"> 8  </button>
-        <button type="button" className="item no-9" name="9"> 9 </button>
-        <button type="button" className="item multiply" name="x"> X </button>
-        <button type="button" className="item no-4" name="4"> 4 </button>
-        <button type="button" className="item no-5" name="5"> 5 </button>
-        <button type="button" className="item no-6" name="6"> 6 </button>
-        <button type="button" className="item substract" name="-"> - </button>
-        <button type="button" className="item no-1" name="1"> 1 </button>
-        <button type="button" className="item no-2" name="2"> 2 </button>
-        <button type="button" className="item no-3" name="3"> 3 </button>
-        <button type="button" className="item add" name="+"> + </button>
-        <button type="button" className="item no-0" name="0"> 0 </button>
-        <button type="button" className="item dot" name="."> . </button>
-        <button type="button" className="item equal" name="="> =  </button>
+        <button type="button" className="item reset" name="AC" onClick={this.eventClick}> AC </button>
+        <button type="button" className="item order" name="+/-" onClick={this.eventClick}> +/- </button>
+        <button type="button" className="item percent" name="%" onClick={this.eventClick}> % </button>
+        <button type="button" className="item divide" name="÷" onClick={this.eventClick}> / </button>
+        <button type="button" className="item no-7" name="7" onClick={this.eventClick}> 7 </button>
+        <button type="button" className="item no-8" name="8" onClick={this.eventClick}> 8  </button>
+        <button type="button" className="item no-9" name="9" onClick={this.eventClick}> 9 </button>
+        <button type="button" className="item multiply" name="x" onClick={this.eventClick}> X </button>
+        <button type="button" className="item no-4" name="4" onClick={this.eventClick}> 4 </button>
+        <button type="button" className="item no-5" name="5" onClick={this.eventClick}> 5 </button>
+        <button type="button" className="item no-6" name="6" onClick={this.eventClick}> 6 </button>
+        <button type="button" className="item substract" name="-" onClick={this.eventClick}> - </button>
+        <button type="button" className="item no-1" name="1" onClick={this.eventClick}> 1 </button>
+        <button type="button" className="item no-2" name="2" onClick={this.eventClick}> 2 </button>
+        <button type="button" className="item no-3" name="3" onClick={this.eventClick}> 3 </button>
+        <button type="button" className="item add" name="+" onClick={this.eventClick}> + </button>
+        <button type="button" className="item no-0" name="0" onClick={this.eventClick}> 0 </button>
+        <button type="button" className="item dot" name="." onClick={this.eventClick}> . </button>
+        <button type="button" className="item equal" name="=" onClick={this.eventClick}> =  </button>
 
       </main>
     );
