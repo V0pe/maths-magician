10f0565b2c1e580bc134cfea2ae68866143c9dd8
V0pe <dampblew01@gmail.com>
2022-05-26 10:32:37 -0400

    pure component rewritten for this.state

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,56 @@
 // eslint-disable-next-line react/prefer-stateless-function
 import React from 'react';
 import './Calculator.css';
+import Calculate from './logic/calculate';
 
-class Calculator extends React.PureComponent {
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+
+    this.eventClick = this.eventClick.bind(this);
+  }
+
+  eventClick(e) {
+    this.setState((state) => ({
+      ...state,
+      ...Calculate(state, e.target.name),
+    }));
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+
     return (
       <main>
-        <div className="item result"> 0 </div>
-        <button type="button" className="item reset" name='AC'> AC </button>
-        <button type="button" className="item order" name='+/-'> +/- </button>
-        <button type="button" className="item percent" name='%'> % </button>
-        <button type="button" className="item divide" name='÷'> / </button>
-        <button type="button" className="item no-7" name='7'> 7 </button>
-        <button type="button" className="item no-8" name='8'> 8  </button>
-        <button type="button" className="item no-9" name='9'> 9 </button>
-        <button type="button" className="item multiply" name='x'> X </button>
-        <button type="button" className="item no-4" name='4'> 4 </button>
-        <button type="button" className="item no-5" name='5'> 5 </button>
-        <button type="button" className="item no-6" name='6'> 6 </button>
+        <div className="item result">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <button type="button" className="item reset" name="AC"> AC </button>
+        <button type="button" className="item order" name="+/-"> +/- </button>
+        <button type="button" className="item percent" name="%"> % </button>
+        <button type="button" className="item divide" name="÷"> / </button>
+        <button type="button" className="item no-7" name="7"> 7 </button>
+        <button type="button" className="item no-8" name="8"> 8  </button>
+        <button type="button" className="item no-9" name="9"> 9 </button>
+        <button type="button" className="item multiply" name="x"> X </button>
+        <button type="button" className="item no-4" name="4"> 4 </button>
+        <button type="button" className="item no-5" name="5"> 5 </button>
+        <button type="button" className="item no-6" name="6"> 6 </button>
         <button type="button" className="item substract" name="-"> - </button>
-        <button type="button" className="item no-1" name='1'> 1 </button>
-        <button type="button" className="item no-2" name='2'> 2 </button>
-        <button type="button" className="item no-3" name='3'> 3 </button>
-        <button type="button" className="item add" name='+'> + </button>
-        <button type="button" className="item no-0" name='0'> 0 </button>
-        <button type="button" className="item dot" name='.'> . </button>
-        <button type="button" className="item equal" name='='> =  </button>
+        <button type="button" className="item no-1" name="1"> 1 </button>
+        <button type="button" className="item no-2" name="2"> 2 </button>
+        <button type="button" className="item no-3" name="3"> 3 </button>
+        <button type="button" className="item add" name="+"> + </button>
+        <button type="button" className="item no-0" name="0"> 0 </button>
+        <button type="button" className="item dot" name="."> . </button>
+        <button type="button" className="item equal" name="="> =  </button>
 
       </main>
     );
