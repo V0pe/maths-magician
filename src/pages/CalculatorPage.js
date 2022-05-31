7110f54c72beb1cb07b627a0eff061e32c5f0980
V0pe <dampblew01@gmail.com>
2022-05-31 08:13:49 -0400

    add calculator page styles

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -3,7 +3,7 @@ import Calculator from '../components/Calculator';
 export default function About() {
   return (
     <>
-      <section>
+      <section className="calculator">
         <h2>Let&#39;s do some maths</h2>
         <Calculator />
       </section>
