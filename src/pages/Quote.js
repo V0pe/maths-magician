71a432e210cc97135ab7b9d27f4449bd032ae618
V0pe <dampblew01@gmail.com>
2022-05-31 08:08:46 -0400

    styled the quote page

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,15 +1,14 @@
 export default function About() {
   return (
     <>
-      <section>
+      <section className="quote">
         <q>
           Mathematics as an expression of the human mind reflects the active will,
           the contemplative reason,
           and the desire for aesthetic perfection.
           Its basic elements are logic and intuition, analysis and construction,
           generality and individuality.
-          —
-          <i>Richard Courant, German-American mathematician </i>
+          <i>—Richard Courant, German-American mathematician </i>
         </q>
       </section>
     </>
