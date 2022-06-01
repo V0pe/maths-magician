26497e182f75198fc713f97c918f0dff336ba3e7
V0pe <dampblew01@gmail.com>
2022-06-01 06:42:46 -0400

    add the react testing library for all button user interaction

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -25,25 +25,25 @@ const Calculator = () => {
         {operation}
         {next}
       </div>
-      <button type="button" className="item reset" name="AC" onClick={eventClick}> AC </button>
-      <button type="button" className="item order" name="+/-" onClick={eventClick}> +/- </button>
-      <button type="button" className="item percent" name="%" onClick={eventClick}> % </button>
-      <button type="button" className="item divide" name="÷" onClick={eventClick}> / </button>
-      <button type="button" className="item no-7" name="7" onClick={eventClick}> 7 </button>
-      <button type="button" className="item no-8" name="8" onClick={eventClick}> 8  </button>
-      <button type="button" className="item no-9" name="9" onClick={eventClick}> 9 </button>
-      <button type="button" className="item multiply" name="x" onClick={eventClick}> X </button>
-      <button type="button" className="item no-4" name="4" onClick={eventClick}> 4 </button>
-      <button type="button" className="item no-5" name="5" onClick={eventClick}> 5 </button>
-      <button type="button" className="item no-6" name="6" onClick={eventClick}> 6 </button>
-      <button type="button" className="item substract" name="-" onClick={eventClick}> - </button>
-      <button type="button" className="item no-1" name="1" data-testid="no-1" onClick={eventClick}> 1 </button>
-      <button type="button" className="item no-2" name="2" onClick={eventClick}> 2 </button>
-      <button type="button" className="item no-3" name="3" onClick={eventClick}> 3 </button>
-      <button type="button" className="item add" name="+" onClick={eventClick}> + </button>
-      <button type="button" className="item no-0" name="0" onClick={eventClick}> 0 </button>
-      <button type="button" className="item dot" name="." onClick={eventClick}> . </button>
-      <button type="button" className="item equal" name="=" onClick={eventClick}> =  </button>
+      <button type="button" className="item reset" name="AC" data-testid="AC" onClick={eventClick}>AC</button>
+      <button type="button" className="item order" name="+/-" data-testid="+/-" onClick={eventClick}>+/-</button>
+      <button type="button" className="item percent" name="%" data-testid="%" onClick={eventClick}>%</button>
+      <button type="button" className="item divide" name="÷" data-testid="/" onClick={eventClick}>/</button>
+      <button type="button" className="item no-7" name="7" data-testid="no-7" onClick={eventClick}>7</button>
+      <button type="button" className="item no-8" name="8" data-testid="no-8" onClick={eventClick}>8</button>
+      <button type="button" className="item no-9" name="9" data-testid="no-9" onClick={eventClick}>9</button>
+      <button type="button" className="item multiply" name="x" data-testid="X" onClick={eventClick}>X</button>
+      <button type="button" className="item no-4" name="4" data-testid="no-4" onClick={eventClick}>4</button>
+      <button type="button" className="item no-5" name="5" data-testid="no-5" onClick={eventClick}>5</button>
+      <button type="button" className="item no-6" name="6" data-testid="no-6" onClick={eventClick}>6</button>
+      <button type="button" className="item substract" name="-" data-testid="-" onClick={eventClick}> - </button>
+      <button type="button" className="item no-1" name="1" data-testid="no-1" onClick={eventClick}>1</button>
+      <button type="button" className="item no-2" name="2" data-testid="no-2" onClick={eventClick}>2</button>
+      <button type="button" className="item no-3" name="3" data-testid="no-3" onClick={eventClick}>3</button>
+      <button type="button" className="item add" name="+"  data-testid="+" onClick={eventClick}>+</button>
+      <button type="button" className="item no-0" name="0" data-testid="0" onClick={eventClick}>0</button>
+      <button type="button" className="item dot" name="." data-testid="." onClick={eventClick}>.</button>
+      <button type="button" className="item equal" name="=" data-testid="=" onClick={eventClick}>= </button>
 
     </main>
   );
--- src/components/___test___/Calculator.test.js
+++ src/components/___test___/Calculator.test.js
@@ -1,20 +1,41 @@
-import React from "react";
-import Calculator from "../Calculator";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Calculator from '../Calculator';
 import '@testing-library/jest-dom/extend-expect';
 
 test('result render 0 initailly', () => {
-    const  { getByTestId }= render(<Calculator />);
-    // const no1 = component.getByTestId('no-1');
-    let result = getByTestId('result');
+  const { getByTestId } = render(<Calculator />);
+  // const no1 = component.getByTestId('no-1');
+  const result = getByTestId('result');
 
-    expect(result.textContent).toBe('0');
-})
+  expect(result.textContent).toBe('0');
+});
 
 test('1 button renders with 1 in result area', () => {
-    const  { getByTestId }= render(<Calculator />);
-    // const no1 = component.getByTestId('no-1');
-    let result = getByTestId('result');
+  const { getByTestId } = render(<Calculator />);
+  const no1 = getByTestId('no-1');
+  const result = getByTestId('result');
+  const no2 = getByTestId('no-2');
+  const no3 = getByTestId('no-3');
+  const no4 = getByTestId('no-4');
+  const no5 = getByTestId('no-5');
+  const no6 = getByTestId('no-6');
+  const no7 = getByTestId('no-7');
+  const no8 = getByTestId('no-8');
+  const no9 = getByTestId('no-9');
+  const no0 = getByTestId('0');
 
-    expect(result.textContent).toBe('0');
-})
+  fireEvent.click(no1);
+  fireEvent.click(no2);
+  fireEvent.click(no3);
+  fireEvent.click(no4);
+  fireEvent.click(no5);
+  fireEvent.click(no6);
+  fireEvent.click(no7);
+  fireEvent.click(no8);
+  fireEvent.click(no9);
+  fireEvent.click(no0);
+
+
+  expect(result.textContent).toBe('1234567890');
+});
