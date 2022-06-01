b3cbdb3c1929adfe102b7070473c5ccc2c4b3782
V0pe <dampblew01@gmail.com>
2022-06-01 08:00:30 -0400

    add renderer dependency

--- src/components/___test___/Calculator.test.js
+++ src/components/___test___/Calculator.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import Calculator from '../Calculator';
 import '@testing-library/jest-dom/extend-expect';
+
+afterEach(() => {
+  cleanup();
+});
 
 test('result render 0 initailly', () => {
   const { getByTestId } = render(<Calculator />);
@@ -10,7 +14,7 @@ test('result render 0 initailly', () => {
   expect(result.textContent).toBe('0');
 });
 
-test('1 button renders with 1 in result area', () => {
+test('1234567890 button renders with 1 in result area', () => {
   const { getByTestId } = render(<Calculator />);
   const no1 = getByTestId('no-1');
   const result = getByTestId('result');
@@ -38,66 +42,58 @@ test('1 button renders with 1 in result area', () => {
   expect(result.textContent).toBe('1234567890');
 });
 
-test('+ button renders with + in result area', () => {
-    const { getByTestId } = render(<Calculator />);
-    const no1 = getByTestId('no-1');
-    const result = getByTestId('result');
-    const no2 = getByTestId('no-2');
-    const no3 = getByTestId('no-3');
-    const add = getByTestId('+');
+test('+ button rendered inbetween number in result area', () => {
+  const { getByTestId } = render(<Calculator />);
+  const no1 = getByTestId('no-1');
+  const result = getByTestId('result');
+  const no2 = getByTestId('no-2');
+  const no3 = getByTestId('no-3');
+  const add = getByTestId('+');
 
-  
-    fireEvent.click(no1);
-    fireEvent.click(no2);
-    fireEvent.click(add);
-    fireEvent.click(no3);
-    
-  
-    expect(result.textContent).toBe('12+3');
-  });
+  fireEvent.click(no1);
+  fireEvent.click(no2);
+  fireEvent.click(add);
+  fireEvent.click(no3);
 
-  test('last opeate button rendered', () => {
-    const { getByTestId } = render(<Calculator />);
-    const no1 = getByTestId('no-1');
-    const result = getByTestId('result');
-    const substract = getByTestId('-');
-    const divide = getByTestId('/');
-    const multiply = getByTestId('X');
+  expect(result.textContent).toBe('12+3');
+});
 
-  
-    fireEvent.click(no1);
-    fireEvent.click(substract);
-    fireEvent.click(divide);
-    fireEvent.click(multiply);
-    
-  
-    expect(result.textContent).toBe('1x');
-  });
+test('last operation button rendered', () => {
+  const { getByTestId } = render(<Calculator />);
+  const no1 = getByTestId('no-1');
+  const result = getByTestId('result');
+  const substract = getByTestId('-');
+  const divide = getByTestId('/');
+  const multiply = getByTestId('X');
 
-  test('AC button rendered with clean slate', () => {
-    const { getByTestId } = render(<Calculator />);
-    const no1 = getByTestId('no-1');
-    const result = getByTestId('result');
-    const substract = getByTestId('-');
-    const divide = getByTestId('/');
-    const percent = getByTestId('%');
-    const AC = getByTestId('AC');
+  fireEvent.click(no1);
+  fireEvent.click(substract);
+  fireEvent.click(divide);
+  fireEvent.click(multiply);
 
-  
-    fireEvent.click(no1);
-    fireEvent.click(substract);
-    fireEvent.click(divide);
-    fireEvent.click(percent);
-    fireEvent.click(AC);
+  expect(result.textContent).toBe('1x');
+});
 
-    
-  
-    expect(result.textContent).toBe('');
+test('AC button rendered with clean slate', () => {
+  const { getByTestId } = render(<Calculator />);
+  const no1 = getByTestId('no-1');
+  const result = getByTestId('result');
+  const substract = getByTestId('-');
+  const divide = getByTestId('/');
+  const percent = getByTestId('%');
+  const AC = getByTestId('AC');
 
-    fireEvent.click(no1);
-    fireEvent.click(substract);
-    fireEvent.click(no1);
+  fireEvent.click(no1);
+  fireEvent.click(substract);
+  fireEvent.click(divide);
+  fireEvent.click(percent);
+  fireEvent.click(AC);
 
-    expect(result.textContent).toBe('1-1');
+  expect(result.textContent).toBe('');
 
-  });
+  fireEvent.click(no1);
+  fireEvent.click(substract);
+  fireEvent.click(no1);
+
+  expect(result.textContent).toBe('1-1');
+});
