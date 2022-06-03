5f68a94ddc0fdb78ec9bc2ab9c5586aaaea382f1
V0pe <dampblew01@gmail.com>
2022-06-03 03:44:40 -0400

    linters fixed

--- src/logic/___tests___/operate.test.js
+++ src/logic/___tests___/operate.test.js
@@ -2,16 +2,16 @@ import operate from '../operate';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('tests for All operations', () => {
-    test('it expects 1 + 3 to be 4', () => {
-      expect(operate(1, 3, '+')).toEqual('4');
-    });
-    test('it expects 4 - 2 to be 2', () => {
-      expect(operate(4, 2, '-')).toEqual('2');
-    });
-    test('it expects 4 x 2 to be 8', () => {
-      expect(operate(4, 2, 'x')).toEqual('8');
-    });
-    test('it expects 4 ÷ 4 to be 1', () => {
-      expect(operate(4, 4, '÷')).toEqual('1');
-    });
+  test('it expects 1 + 3 to be 4', () => {
+    expect(operate(1, 3, '+')).toEqual('4');
   });
+  test('it expects 4 - 2 to be 2', () => {
+    expect(operate(4, 2, '-')).toEqual('2');
+  });
+  test('it expects 4 x 2 to be 8', () => {
+    expect(operate(4, 2, 'x')).toEqual('8');
+  });
+  test('it expects 4 ÷ 4 to be 1', () => {
+    expect(operate(4, 4, '÷')).toEqual('1');
+  });
+});
